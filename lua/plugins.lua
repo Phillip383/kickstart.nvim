e76ec65f01915f82b961d59c0e8539db0bdeb4bb
Phillip Wood <Phillipwood2023@outlook.com>
2025-11-04 05:21:44 -0500

    Updated lsp config framework.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -416,31 +416,18 @@ require('lazy').setup({
       --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
       local capabilities = vim.lsp.protocol.make_client_capabilities()
       capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
-      require('lspconfig').gdscript.setup(capabilities)
-      -- Enable the following language servers
-      --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
-      --
-      --  Add any additional override configuration in the following tables. Available keys are:
-      --  - cmd (table): Override the default command used to start the server
-      --  - filetypes (table): Override the default list of associated filetypes for the server
-      --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
-      --  - settings (table): Override the default settings passed when initializing the server.
-      --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
+      
+      local on_attach = function(client, bufnr)
+        vim.notify(string.format("LSP client attached: %s", client.name))
+      end
+
       local servers = {
         clangd = {},
         gopls = {},
         pyright = {},
         rust_analyzer = {},
         omnisharp = {},
-        --gdscript = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
-        --
+        gdscript = {},
 
         lua_ls = {
           -- cmd = { ... },
@@ -462,6 +449,22 @@ require('lazy').setup({
           },
         },
       }
+
+      -- vim.lsp.config("gdscript", {
+      --   capabilities = capabilities,
+      --   on_attach = on_attach
+      -- })
+
+      for server, config in pairs(servers) do
+        local final_config = vim.tbl_deep_extend('force', {
+          capabilities = capabilities,
+          on_attach = on_attach
+        }, config)
+
+        vim.lsp.config(server, final_config)
+
+        vim.lsp.enable(server)
+      end
 
       -- Ensure the servers and tools above are installed
       --
